a9705b02782214b700cc998678a80088a0e2748d
Devin Despain <devin@dspa.in>
2015-10-20 02:26:22 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ah-email-utils-plugin
+
+This plugin will add email formatting middleware to an ActionHero API Server
+
+## Setup
+
+- install this plugin: `npm install ah-email-utils-plugin --save`
---
+++ initializers/ah-email-utils-plugin.js
@@ -0,0 +1,35 @@
+var _ = require('underscore');
+var validator = require('validator');
+
+module.exports = {
+  initialize : function(api, next){
+
+    _.each(api.actions.actions, function(action){
+      _.each(action, function(actionTemplate, version){
+        var emailFields = _.pick(actionTemplate.inputs, 'email', 'emailAddress', 'emailaddress', 'email-address');
+        _.each(emailFields, function(input){
+          if((!input.validator && !_.isBoolean(input.validator)) || (_.isBoolean(input.validator) && input.validator)){
+            input.validator = function(param){
+              if(validator.normalizeEmail(param)){
+                return true
+              }else{
+                var hasConfig = _.isFunction(api.config.errors.invalidEmail)
+                var error = 'invalid email';
+                if(hasConfig) error = api.config.errors.invalidEmail(param); 
+                return new Error( error )
+              }
+            }
+          }
+
+          if((!input.formatter && !_.isBoolean(input.formatter)) || (_.isBoolean(input.formatter) && input.formatter)){
+            input.formatter = function(param){
+              return validator.normalizeEmail(param)
+            }
+          }
+        })
+      })
+    })
+    
+    next();
+  }
+};
