b30172503f4488c07ccb7abb77ffaa305376a9cb
Devin Despain <devin@dspa.in>
2015-10-20 02:32:25 -0600

    returning the actual field name in the error message

--- initializers/ah-email-utils-plugin.js
+++ initializers/ah-email-utils-plugin.js
@@ -7,14 +7,14 @@ module.exports = {
     _.each(api.actions.actions, function(action){
       _.each(action, function(actionTemplate, version){
         var emailFields = _.pick(actionTemplate.inputs, 'email', 'emailAddress', 'emailaddress', 'email-address');
-        _.each(emailFields, function(input){
+        _.each(emailFields, function(input, fieldName){
           if((!input.validator && !_.isBoolean(input.validator)) || (_.isBoolean(input.validator) && input.validator)){
             input.validator = function(param){
               if(validator.normalizeEmail(param)){
                 return true
               }else{
                 var hasConfig = _.isFunction(api.config.errors.invalidEmail)
-                var error = 'invalid email';
+                var error = 'invalid ' + fieldName;
                 if(hasConfig) error = api.config.errors.invalidEmail(param); 
                 return new Error( error )
               }
